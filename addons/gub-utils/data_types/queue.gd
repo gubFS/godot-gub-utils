09ab29c0808233efd2c51fd647d7646ec4151d5d
gubFS <229577543+gubFS@users.noreply.github.com>
2025-10-06 11:23:41 +0200

    Rework queue to have better naming and more functions

--- addons/gub-utils/data_types/queue.gd
+++ addons/gub-utils/data_types/queue.gd
@@ -3,14 +3,40 @@ class_name Queue
 
 var _elements : Array[Variant] = []
 
-func add(element : Variant) -> void:
+func enqueue(element : Variant) -> void:
 	_elements.push_back(element)
 
-func take() -> Variant:
+## Synonym for enqueue
+func push(element : Variant) -> void:
+	enqueue(element)
+
+
+func dequeue() -> Variant:
 	return _elements.pop_front()
 
-func peek() -> Variant:
+## Synonym for dequeue
+func pop() -> Variant:
+	return dequeue()
+
+## Returns null if empty
+func front() -> Variant:
 	if(_elements.is_empty()):
 		return null
 	
 	return _elements.front()
+
+## Synonym for front
+func peek() -> Variant:
+	return front()
+
+func clear() -> void:
+	_elements.clear()
+
+func size() -> int:
+	return _elements.size()
+
+func is_empty() -> bool:
+	return _elements.is_empty()
+
+func _to_string() -> String:
+	return str(_elements)
