b7f6befc4b379eec5aa236c89308db5e7f78b694
gubFS <229577543+gubFS@users.noreply.github.com>
2025-10-08 16:41:50 +0200

    update Task to use static methods

--- addons/gub-utils/csharp/task.gd
+++ addons/gub-utils/csharp/task.gd
@@ -1,29 +1,35 @@
 class_name Task
 extends RefCounted
 
-signal on_any
-signal on_all
-var on_all_counter: int = 0
 
-static func from() -> Task:
-	return Task.new()
+class TaskWhenAny:
+	signal on_any
+	func when_any(signals: Array) -> void:
+		for signal_: Signal in signals:
+			var callable: Callable = func() -> void: on_any.emit()
+			signal_.connect(callable)
+			on_any.connect(func() -> void: signal_.disconnect(callable))
 
-func when_any(...signals: Array) -> Signal:
-	for signal_: Signal in signals:
-		var callable: Callable = func() -> void: on_any.emit()
-		signal_.connect(callable)
-		on_any.connect(func() -> void: signal_.disconnect(callable))
+		await on_any
 
-	return on_any
+static func when_any(signals: Array) -> void:
+	var task: TaskWhenAny = TaskWhenAny.new()
+	await task.when_any(signals)
 
-func when_all(...signals: Array) -> Signal:
-	on_all_counter = signals.size()
-	for signal_: Signal in signals:
-		var callable: Callable = func() -> void: 
-			on_all_counter -= 1
-			if on_all_counter == 0:
-				on_all.emit()
-		signal_.connect(callable, CONNECT_ONE_SHOT)
+class TaskWhenAll:
+	signal on_all
+	var on_all_counter: int = 0
+	func when_all(signals: Array) -> void:
+		on_all_counter = signals.size()
+		for signal_: Signal in signals:
+			var callable: Callable = func() -> void: 
+				on_all_counter -= 1
+				if on_all_counter == 0:
+					on_all.emit()
+			signal_.connect(callable, CONNECT_ONE_SHOT)
 
-	return on_all
+		await on_all
 
+static func when_all(signals: Array[Signal]) -> void:
+	var task: TaskWhenAll = TaskWhenAll.new()
+	await task.when_all(signals)
