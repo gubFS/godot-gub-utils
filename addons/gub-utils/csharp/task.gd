63f3913e2685d394afb7ca44bbace4d3a917ce2d
gubFS <229577543+gubFS@users.noreply.github.com>
2025-10-11 14:10:29 +0200

    fix: add an ignored array of args in tasks, in case a signal is emitted with arguments

--- addons/gub-utils/csharp/task.gd
+++ addons/gub-utils/csharp/task.gd
@@ -6,7 +6,7 @@ class TaskWhenAny:
 	signal on_any
 	func when_any(signals: Array) -> void:
 		for signal_: Signal in signals:
-			var callable: Callable = func() -> void: on_any.emit()
+			var callable: Callable = func(..._args: Array) -> void: on_any.emit()
 			signal_.connect(callable)
 			on_any.connect(func() -> void: signal_.disconnect(callable))
 
@@ -22,7 +22,7 @@ class TaskWhenAll:
 	func when_all(signals: Array) -> void:
 		on_all_counter = signals.size()
 		for signal_: Signal in signals:
-			var callable: Callable = func() -> void: 
+			var callable: Callable = func(..._args: Array) -> void: 
 				on_all_counter -= 1
 				if on_all_counter == 0:
 					on_all.emit()
