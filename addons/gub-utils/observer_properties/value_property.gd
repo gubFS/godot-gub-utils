4dd0e266056d0e05b5422220aaeb183a43425616
gubFS <229577543+gubFS@users.noreply.github.com>
2025-10-17 09:18:44 +0200

    add the ability to reuse connection id for disconnection conneciton in observer property

--- addons/gub-utils/observer_properties/value_property.gd
+++ addons/gub-utils/observer_properties/value_property.gd
@@ -7,7 +7,7 @@ var value : Variant:
 		_emit_changed()
 
 var next_connection_id: int = 0
-var connections: Dictionary[int, Array] = {}
+var _connections: Dictionary[int, Array] = {}
 
 @warning_ignore("untyped_declaration") 
 func _init(value_ = null) -> void:
@@ -16,22 +16,27 @@ func _init(value_ = null) -> void:
 func get_value() -> Variant:
 	return value
 
-func _add_connection(...callables: Array) -> int:
+func get_unique_connection_id() -> int:
 	var connection_id: int = next_connection_id
 	next_connection_id += 1
-	connections[connection_id] = callables
+	return connection_id
+
+func _add_connection(callable: Callable, connection_id: int = -1) -> int:
+	if connection_id == -1: connection_id = get_unique_connection_id()
+	_connections.get_or_add(connection_id, [])
+	_connections[connection_id].append(callable)
 	return connection_id
 
 ## Calls the callable with the value as a parameter, whenever the value is changed. 
 ## Also calls it immdeiatly in order to initialize the state. Use the changed signal if unwanted.
-func on_changed(callable : Callable) -> int:
+func on_changed(callable : Callable, connection_id: int = -1) -> int:
 	changed.connect(callable)
 	callable.call(value)
-	return _add_connection(callable)
+	return _add_connection(callable, connection_id)
 
 ## Whenever this property changes call callable, but disconnect bidirectional_signal first. Whenever bidirectional_signal emits, call bidirectional_callable, but disconnect this property first.
 ## Callable will have the new value passed as an argument, and bidirectional_callable will have whatever arguments bidirectional_signal emitted with.
-func on_changed_bidirectional(callable: Callable, bidirectional_signal: Signal, bidirectional_callable: Callable) -> int:
+func on_changed_bidirectional(callable: Callable, bidirectional_signal: Signal, bidirectional_callable: Callable, connection_id: int = -1) -> int:
 	# Don't mind the hackyness here, surely theres gotta be a better way?
 	var box: ValueBox = ValueBox.new()
 	
@@ -47,15 +52,15 @@ func on_changed_bidirectional(callable: Callable, bidirectional_signal: Signal, 
 	box.set_value(bidirectional_callable)
 
 	bidirectional_signal.connect(bidirectional_callable)
-	var connection_id: int = on_changed(callable)
-	connections[connection_id].append(bidirectional_callable)
+	connection_id = on_changed(callable, connection_id)
+	_add_connection(bidirectional_callable, connection_id)
 	return connection_id
 
 ## Whenever the value changes, the given property on the given object will also be set to the value.
-func bind(obj: Object, property: String) -> int:
+func bind(obj: Object, property: String, connection_id: int = -1) -> int:
 	var bind_callable: Callable = func(value_: Variant) -> void:
 		obj.set(property, value_)
-	return on_changed(bind_callable)
+	return on_changed(bind_callable, connection_id)
 
 ## Whenever either the ValueProperty or the property of the object is changed, they both match to equal eachother.
 ## A signal that emits whenever a property is changed, must be passed. The singal should emit like so: ``_on_set.emit(property_name: StringName, property_value: Variant)``
@@ -67,7 +72,7 @@ func bind(obj: Object, property: String) -> int:
 ## ```
 ## And for variables within the class emit the signal, in a custom setter. (remember to also set the value)
 ## If changing a inherited variable from whithin the class, it will not call _set(), use self.my_var to use _set(), and emit the signal properly.
-func bind_bidirectional(obj: Object, property: String, on_set_signal: Signal) -> int:
+func bind_bidirectional(obj: Object, property: String, on_set_signal: Signal, connection_id: int = -1) -> int:
 	var bind_callable: Callable = func(value_: Variant) -> void:
 		obj.set(property, value_)
 	
@@ -75,11 +80,12 @@ func bind_bidirectional(obj: Object, property: String, on_set_signal: Signal) ->
 		if property_name == property:
 			value = property_value
 
-	return on_changed_bidirectional(bind_callable, on_set_signal, bidirectional_callable)
+	return on_changed_bidirectional(bind_callable, on_set_signal, bidirectional_callable, connection_id)
 
 func remove_connection(connection_id: int) -> void:
-	for callable: Callable in connections[connection_id]:
+	for callable: Callable in _connections[connection_id]:
 		changed.disconnect(callable)
+	_connections[connection_id].clear()
 
 func _to_string() -> String:
 	return str(value)
